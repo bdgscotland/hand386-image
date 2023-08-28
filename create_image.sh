5fd2dcdbc371f6a904023a7e29b93f28a3c5bd81
Duncan <duncan@bowring.us>
2023-08-27 20:40:55 -0700

    SUCCESSFUL AUTO INSTALLATION OF DOS 622 + WIN 311

--- create_image.sh
+++ create_image.sh
@@ -26,7 +26,7 @@ done
 ### CREATE IMAGE
 
 echo "Creating Image..."
-dd if=/dev/zero of=working/dos622.img bs=1m count=1024
+dd if=/dev/zero of=working/dos622.img bs=512k count=2048
 
 ### FORMAT IMAGE
 echo "Formatting Image..."
@@ -106,8 +106,6 @@ rm working/dos622.img
 
 echo "Installing DOS 6.22..."
 
-### Install DOS 6.22
-
 {
     sleep 5;            # Wait for DOS installation to start
     echo "sendkey ret"; # Press Enter
@@ -146,3 +144,82 @@ echo "Installing DOS 6.22..."
 } | qemu-system-i386 -fda software/dos622/Disk1.img -hda hand386_v0.img -boot a -monitor stdio
 
 echo "DOS 6.22 Installation Complete."
+
+### Install Windows 3.11
+echo "Installing Windows 3.11..."
+
+### Install Windows 3.11
+
+{
+    sleep 5;               # Wait a bit before starting commands
+
+    echo "sendkey a";          # Press 'a'
+    echo "sendkey shift-semicolon"; # Press ':'
+    echo "sendkey ret";        # Press Enter
+    sleep 2;               # Assume a small wait
+
+    # Start typing 'setup'
+    echo "sendkey s";
+    echo "sendkey e";
+    echo "sendkey t";
+    echo "sendkey u";
+    echo "sendkey p";
+    echo "sendkey ret";        # Press Enter to execute `setup`
+    sleep 10;              # Assume a wait time for setup to initialize
+
+    echo "sendkey ret";        # Press Enter
+    echo "sendkey ret";        # Press Enter
+    sleep 5;               # Some wait time
+    
+    echo "eject -f floppy0";   # Eject current floppy image
+    echo "change floppy0 software/win311/DISK2.IMG";   # Insert DISK2
+    sleep 5;              # Wait a bit
+    echo "sendkey ret";        # Press Enter
+    sleep 10;              # Wait a bit
+
+    echo "eject -f floppy0";   # Eject DISK2
+    echo "change floppy0 software/win311/DISK3.IMG";   # Insert DISK3    
+    sleep 5;              # Wait a bit
+    echo "sendkey ret";        # Press Enter
+    sleep 10;              # Wait a bit
+    
+    # Start typing 'hand386'
+    echo "sendkey h";
+    echo "sendkey a";
+    echo "sendkey n";
+    echo "sendkey d";
+    echo "sendkey 3";
+    echo "sendkey 8";
+    echo "sendkey 6";
+    echo "sendkey ret";        # Press Enter to finalize the input
+    sleep 5;               # Some wait time
+
+    echo "sendkey ret";        # Press Enter
+    echo "sendkey ret";        # Press Enter
+    sleep 15;
+    
+    echo "eject -f floppy0";   # Eject current floppy image
+    echo "change floppy0 software/win311/DISK4.IMG";   # Insert DISK4
+                      # Wait a bit    
+    echo "sendkey ret";        # Press Enter
+    sleep 15;              # Wait a bit
+    
+
+    echo "eject -f floppy0";   # Eject DISK4
+    echo "change floppy0 software/win311/DISK5.IMG";   # Insert DISK5
+    echo "sendkey ret";        # Press Enter
+    sleep 15;              # Wait a bit
+
+    echo "sendkey ret";        # Press Enter
+    echo "sendkey alt-s";      # Press Alt+S (This command might depend on the specific configuration)
+    echo "sendkey ret";        # Press Enter
+
+    sleep 15;              # Wait a bit
+    echo "quit";           # Quit QEMU
+
+} | qemu-system-i386 -fda software/win311/DISK1.IMG -hda hand386_v0.img -boot c -monitor stdio
+
+
+# Test Boot
+#qemu-system-i386 -fda software/win311/DISK1.IMG -hda hand386_v0.img -boot c -monitor stdio
+
